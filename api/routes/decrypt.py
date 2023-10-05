bfd4e7d7d6fc9b8c4248d4455fc126499681edcf
Nikolay Simakov <n.simakoff@gmail.com>
2023-10-05 19:44:23 +0300

    Add decrypt route

--- api/routes/decrypt.py
+++ api/routes/decrypt.py
@@ -1,7 +1,25 @@
-from fastapi import APIRouter, status
+from http import HTTPStatus
+from fastapi import APIRouter, status, HTTPException
+
+from models import decrypt
+from services.ciphers import sipher_determiner
+
+
 router = APIRouter()
 
 
-@router.post('/')
-def process_decrypt_data(data):
-    pass
+@router.post('/json/string')
+def process_decrypt_data(data: decrypt.DecryptRequest) -> decrypt.DecryptResponse:
+    try:
+        sd = sipher_determiner(data.key)
+        res = sd(data.body)['algorithm']
+        return decrypt.DecryptResponse(
+            info=res,
+        )
+    except:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail={
+                "error": "Invalid request",
+            },
+        )
